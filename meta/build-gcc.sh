6cd90adb799133c9e7866b7bbbf504ab84c71b08
Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>
2024-03-09 22:45:39 -0300

    Execute meta/build-gcc.sh with bash
    Signed-off-by: Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>

--- meta/build-gcc.sh
+++ meta/build-gcc.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export PREFIX="$(pwd)/cross/dest"
 export TARGET="x86_64-elf"
@@ -15,9 +15,9 @@ sed '1902i tmake_file="${tmake_file} i386/t-x86_64-elf"' -i gcc/config.gcc
 mkdir build
 cd build
 
-../configure --target="$TARGET" --prefix="$PREFIX" --disable-nls --enable-languages=c --without-headers
+../configure --target="$TARGET" --prefix="$PREFIX" --disable-nls --enable-languages=c --without-headers && \
 
-make -j$(nproc) all-gcc
-make -j$(nproc) all-target-libgcc
-make install-gcc
+make -j$(nproc) all-gcc           && \
+make -j$(nproc) all-target-libgcc && \
+make install-gcc                  && \
 make install-target-libgcc
