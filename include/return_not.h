b88760013759c4382077ee88bb0dc914887a0024
Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>
2024-03-13 15:38:13 -0300

    Add inclusion for ktext.h in return_not.h
    Signed-off-by: Vinícius Schütz Piva <vinicius.vsczpv@outlook.com>

--- include/return_not.h
+++ include/return_not.h
@@ -1,6 +1,7 @@
 #ifndef KERNEL_RETURN_NOT_H_
 #define KERNEL_RETURN_NOT_H_
 
+#include <ktext.h>
 #include <panic.h>
 
 #define return_not { panic("return_not reached."); for (;;) {}; kprintf("### !!!EXTREME BUG!!! ### return_not in %s:%i bypassed.\n", __FILE__, __LINE__); }
